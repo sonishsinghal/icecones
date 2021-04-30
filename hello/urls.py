e633fa8ffb54495b3a13767e5b61c67583ef782f
sonishsinghal <sonishsinghal@gmail.com>
2021-04-30 22:13:03 +0530

    commited

--- hello/urls.py
+++ hello/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = "sonish Ice Cream "
 admin.site.site_title = "sonish Ice Cream  Portal"
@@ -23,4 +27,6 @@ admin.site.index_title = "Welcome to sonish Ice Cream Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
